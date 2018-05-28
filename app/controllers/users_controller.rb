fda355b6b8fe04bd6e88cb5a58bd527e925c22c7
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-05-28 05:44:42 +0000

    user task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class UsersController < ApplicationController
+=begin
   before_action :require_user_logged_in, only: [:index, :show]
   def index
   end
@@ -7,7 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+=end
   def new
     @user = User.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_082457) do
+ActiveRecord::Schema.define(version: 2018_05_28_023942) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -25,6 +27,10 @@ ActiveRecord::Schema.define(version: 2018_05_25_082457) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "task_id"
+    t.index ["task_id"], name: "index_users_on_task_id"
   end
 
+  add_foreign_key "tasks", "users"
+  add_foreign_key "users", "tasks"
 end
