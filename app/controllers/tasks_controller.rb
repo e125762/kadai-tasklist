eae9c70248651a376e4dbcd953111f5ab3b724e8
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-05-28 08:31:51 +0000

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
 
   def index
     if logged_in?
--- db/migrate/20180528023942_add_task_ref_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTaskRefToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :users, :task, foreign_key: true
-  end
-end
